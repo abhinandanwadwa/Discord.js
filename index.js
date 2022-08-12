63158d0414bd2d0f266f8eff171e583a1a918164
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-12 13:29:56 +0530

    Basic Boiler Plate for Event Handling with command handling

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 
-client.once('ready', (c) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', interaction => {
-    console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered and interaction!!`);
-});
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    }
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+    // After this, listening for other events is as easy as creating a new file in the events folder. The event handler will automatically retrieve and register it whenever you restart your bot.
+
+
+    // In most cases, you can access your client instance in other files by obtaining it from one of the other discord.js structures, e.g. interaction.client in the interactionCreate event.
+}
+
+// client.once('ready', (c) => {
+//     console.log(`Ready! Logged in as ${c.user.tag}`);
+// });
+
+// client.on('interactionCreate', interaction => {
+//     console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered and interaction!!`);
+// });
 
 client.login(token);
