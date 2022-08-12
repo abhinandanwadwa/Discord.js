6763cb2b18458ddbefa26e66a522d2c252dd121b
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-12 12:46:53 +0530

    Basic Discord Bot Boiler Plate with command handling

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new Client Instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (ONLY ONCE)
-client.on('ready', () => {
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    const { commandName } = interaction;
+    const command = client.commands.get(interaction.commandName);
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
     }
-    else if (commandName === 'server') {
-        await interaction.reply(`Server Name: ${interaction.guild.name}\nTotal Members: ${interaction.guild.memberCount}`);
+    catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
-    else if (commandName === 'user') {
-        await interaction.reply(`Your Tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-    }
+
+    // And that's it! Whenever you want to add a new command, make a new file in your commands directory, name it the same as the slash command, and then do what you did for the other commands. Remember to run node deploy-commands.js to register your commands!
 });
 
-// Login to Discord with your client's token
 client.login(token);
